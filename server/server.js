387400d95333165d67f0584ff45e20b0690bfdf7
adam-james <adam.j.sulewski@gmail.com>
2016-03-06 22:22:04 -0800

    remove unnecessary links

--- server/server.js
+++ server/server.js
@@ -47,15 +47,13 @@ function renderFullPage(html, initialState) {
     <head>
       <meta charset="UTF-8">
       <title>Twain a Day</title>
-      <!-- <link href='https://fonts.googleapis.com/css?family=Quicksand|Rock+Salt' rel='stylesheet' type='text/css'>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     </head>
     <body>
       <div id ="root">${html}</div>
       <script>
         window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
       </script>
-      <!-- <script type="text/javascript" src="http://localhost:8080/dist/bundle.js"></script> -->
       <script type="text/javascript" src="/static/bundle.js"></script>
     </body>
     </html>

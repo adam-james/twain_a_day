645f12596fca3d162ced40c4078873014772514d
adam-james <adam.j.sulewski@gmail.com>
2016-03-08 00:54:02 -0800

    set up client store

--- client/index.js
+++ client/index.js
@@ -1,3 +1,15 @@
 import './scss/application.scss'
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from '../common/store/configureStore'
+import App from '../common/containers/App'
+
+const store = configureStore(window.__INITIAL_STATE__)
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
